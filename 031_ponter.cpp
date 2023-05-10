111044908dd00153ebaf54015fab4fa0dc62742e
Izalhadji31 <ryzalhadji@gmail.com>
2023-05-10 15:11:47 +0700

    menambahkan coment pada class mahasiswa

--- 031_ponter.cpp
+++ 031_ponter.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class mahasiswa {
 public:
     int nim;
-    void showNim();
+    void showNim(); //deklarasi method
 };
 void mahasiswa::showNim() {
     cout << "No Induk = " << nim << endl;
