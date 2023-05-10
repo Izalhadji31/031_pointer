fcbd75edfddc82998505ee271732a8cd4318e7bf
Izalhadji31 <ryzalhadji@gmail.com>
2023-05-10 15:24:56 +0700

    class mahasiswa pada pointer 2  telah selesai

--- 031_pointer2/031_pointer2.cpp
+++ 031_pointer2/031_pointer2.cpp
@@ -2,9 +2,14 @@
 using namespace std;
 
 class mahasiswa {
-
+public:
+    int nim;
+    void showNim() {
+        cout << "No Induk = " << nim << endl;
+    }
 };
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
